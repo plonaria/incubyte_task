4dd254d5abe655f64fb87434cd8d78ce65f230cd
padmaja <padmajalonaria31@gmail.com>
2025-09-04 10:25:19 +0530

    failing test for many numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,5 +13,9 @@ RSpec.describe StringCalculator do
     it 'returns sum for two comma-separated numbers' do
 		  expect(StringCalculator.add('1,5')).to eq(6)
 		end
+
+		it 'sums any amount of comma-separated numbers' do
+			expect(StringCalculator.add('1,2,3,4').to eq(10))
+		end
   end
 end
