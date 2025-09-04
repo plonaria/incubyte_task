e4648490ee69f80f91c6880f3afe00605bd2b332
padmaja <padmajalonaria31@gmail.com>
2025-09-04 10:22:41 +0530

    handle two numbers with delimiter

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -2,5 +2,12 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers == ""
     return numbers.to_i if numbers.to_i.to_s == numbers
+
+    if numbers.include?(',')
+      a, b = numbers.split(',', 2)
+      return a.to_i + b.to_i
+    end
+
+    return numbers.to_i
   end
 end
