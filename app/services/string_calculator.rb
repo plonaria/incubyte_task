5ab3dba8b38bdecf7be4877e056c8a743fac8b0f
padmaja <padmajalonaria31@gmail.com>
2025-09-04 10:37:28 +0530

    newline and commas as delimiter

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,9 +1,14 @@
 class StringCalculator
+	DEFAULT_DELIMITER = [',', "\n"].freeze
+
   def self.add(numbers)
     return 0 if numbers == ""
     return numbers.to_i if numbers.to_i.to_s == numbers
 
-    parts = numbers.split(',')
-    return parts.map(&:to_i).sum
+    parts = numbers.split(Regexp.union(DEFAULT_DELIMITER))
+    parts.map!(&:to_s)
+    parts.reject!(&:empty?)
+    parts.map!(&:to_i)
+    return parts.sum
   end
 end
