2e377c6e12aaa3b52819151a6b6545aa70e84b52
padmaja <padmajalonaria31@gmail.com>
2025-09-04 10:50:56 +0530

    removed commented lines

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -5,12 +5,6 @@ class StringCalculator
     return 0 if numbers == ""
     return numbers.to_i if numbers.to_i.to_s == numbers
 
-    # parts = numbers.split(Regexp.union(DEFAULT_DELIMITER))
-    # parts.map!(&:to_s)
-    # parts.reject!(&:empty?)
-    # parts.map!(&:to_i)
-    # return parts.sum
-
       # detect custom delimiter
     delimiter = /,|\n/
     if numbers.start_with?('//')
